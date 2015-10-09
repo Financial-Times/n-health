bff0afe248010aa8e798b39c79571f54ea082f62
Paul I Wilson <paul.i.wilson@ft.com>
2015-10-09 14:13:43 +0100

    bugfixes

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 'use strict';
-require('babel/register');
-const startup = require('./src/startup');
+try{
+	require('babel/register');
+}catch(e){
+	// babel already loaded
+}
 
-module.exports = startup;
+const startup = require('./src/startup');
+const Check = require('./src/checks/check');
+const status = require('./src/checks/status');
+
+function categoryInCheckName(healthCheck, check){
+	check.name = healthCheck.name + ': ' + check.name;
+	return check;
+}
+
+module.exports = function(config, additionalChecks){
+	let healthCheckMap = startup(config, additionalChecks);
+	return{
+		asMap: () => healthCheckMap,
+		asArray: () => {
+			let healthCheckArray = [];
+			for (let healthCheck of healthCheckMap.values()) {
+				let checks = healthCheck.checks.map(categoryInCheckName.bind(null, healthCheck));
+				healthCheckArray = healthCheckArray.concat(checks);
+			}
+
+			return healthCheckArray;
+		}
+	}
+};
+
+module.exports.Check = Check;
+module.exports.status = status;
