70bf45cbcd513e89c4431e48dc7846e1ac2364d8
Paul I Wilson <paul.i.wilson@ft.com>
2016-04-19 16:49:22 +0100

    heroku adaptor

---
+++ src/lib/herokuAdaptor.js
@@ -0,0 +1,51 @@
+'use strict';
+const ms = require('ms');
+const fetch = require('node-fetch');
+
+if(!process.env.HEROKU_AUTH_TOKEN){
+	throw new Error('HEROKU_AUTH_TOKEN env var required');
+}
+
+let url = '/dyno/errors?process_type=web&start_time=2016-04-18T09%3A30%3A00.000Z&end_time=2016-04-19T09%3A20%3A00.000Z&step=10m';
+
+function getErrorMetrics(app, duration){
+	let durationMs = ms(duration);
+	let endTime = new Date();
+	let startTime = new Date(endTime - durationMs);
+	let step = ms(durationMs / 10);
+	let url = `https://api.metrics.herokai.com/metrics/${app}/dyno/errors?process_type=web&start_time=${startTime.toISOString()}&end_time=${endTime.toISOString()}&step=${step}`;
+	return fetch(url,
+		{
+			headers:{
+				'Authorization': 'Bearer ' + process.env.HEROKU_AUTH_TOKEN
+			}
+		}
+	)
+		.then(response => {
+			if(!response.ok){
+				let err = new Error(`Failed to fetch ${url}.  Response was ${response.status}`);
+				err.type = 'HEROKU_API_ERROR';
+			}
+
+			return response.json();
+		});
+}
+
+function getTotalErrorCount(data, code){
+	let errors = data.data[code];
+	if(!errors || !errors.length || !errors.reduce){
+		return 0;
+	}
+
+	return errors.reduce((total, count) => {
+		return count ? total + count : total;
+	}, 0);
+}
+
+module.exports = {
+	getErrorMetrics: getErrorMetrics,
+	getR14Count: (app, duration) => {
+		return getErrorMetrics(app, duration)
+			.then(data => getTotalErrorCount(data, 'R14'))
+	}
+};
--- test/memcheck.spec.js
+++ test/memcheck.spec.js
@@ -3,7 +3,7 @@ const expect = require('chai').expect;
 const sinon = require('sinon');
 const proxyquire = require('proxyquire').noCallThru().noPreserveCache();
 
-describe.only('Memory Usage Check', function(){
+describe('Memory Usage Check', function(){
 
 
 	function wait(ms){
