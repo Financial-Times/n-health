3331a2123452bcdb518364998f5ac95dfd2c5d2f
Paul I Wilson <paul.i.wilson@ft.com>
2016-04-21 11:04:17 +0100

    extract integration test

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@ include n.Makefile
 .PHONY: test
 
 test-unit:
-	export HOSTEDGRAPHITE_READ_APIKEY=test-graph-key; mocha -r loadvars.js
+	export HOSTEDGRAPHITE_READ_APIKEY=test-graph-key; export HEROKU_AUTH_TOKEN=token; mocha
+
+test-int:
+	mocha int-tests/ -r loadvars.js
 
 test: verify test-unit
--- test/herokuAdaptor.spec.js
+++ test/herokuAdaptor.spec.js
@@ -4,48 +4,23 @@ const sinon = require('sinon');
 const proxyquire = require('proxyquire').noCallThru().noPreserveCache();
 
 describe('Heroku Adaptor', function(){
-
-	const wait = ms => new Promise(r => setTimeout(r, ms));
+	
 	let adaptor;
+	let fixture = require('./fixtures/herokuMetricsApiResponse.json');
+	let mockResponse = {status:200, ok:true, json:() => fixture};
+	let mockFetch = sinon.stub().returns(Promise.resolve(mockResponse));
 
-	describe('Itegration', () => {
-
-		before(() => {
-			adaptor = require('../src/lib/herokuAdaptor');
-		});
-
-		it('Should be able to get data from the metrics api', () => {
-			return adaptor.getErrorMetrics('ft-next-article-eu', '10m')
-				.then(metrics => {
-					expect(metrics).to.exist;
-					expect(metrics).to.have.property('start_time');
-					expect(metrics).to.have.property('end_time');
-					expect(metrics).to.have.property('step');
-					expect(metrics).to.have.property('data');
-				})
-		});
-
+	before(() => {
+		adaptor = proxyquire('../src/lib/herokuAdaptor', {'node-fetch':mockFetch});
 	});
 
-	describe('Unit tests', () => {
 
-		let adaptor;
-		let fixture = require('./fixtures/herokuMetricsApiResponse.json');
-		let mockResponse = {status:200, ok:true, json:() => fixture};
-		let mockFetch = sinon.stub().returns(Promise.resolve(mockResponse));
-
-		before(() => {
-			adaptor = proxyquire('../src/lib/herokuAdaptor', {'node-fetch':mockFetch});
-		});
-
-
-		it('Should return the number of R14 errors within the given timeframe', () => {
-			return adaptor.getR14Count('ft-next-article-eu', '10m')
-				.then(count => {
-					expect(count).to.exist;
-					expect(count).to.equal(5953);
-				})
-		});
-
+	it('Should return the number of R14 errors within the given timeframe', () => {
+		return adaptor.getR14Count('ft-next-article-eu', '10m')
+			.then(count => {
+				expect(count).to.exist;
+				expect(count).to.equal(5953);
+			})
 	});
+
 });
