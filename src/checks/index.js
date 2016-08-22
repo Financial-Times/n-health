9e39be668920ef14f5b83cf9599659b15559bc91
fahad.khan <fahad.khan@ft.com>
2016-08-22 15:15:11 +0100

    include string check in index.js to make it usable

--- src/checks/index.js
+++ src/checks/index.js
@@ -4,6 +4,7 @@ module.exports = {
 	aggregate : require('./aggregate.check'),
 	responseCompare : require('./responseCompare.check'),
 	json : require('./json.check'),
+	string : require('./string.check'),
 	pingdom : require('./pingdom.check'),
 	graphiteSpike: require('./graphiteSpike.check'),
 	graphiteWorking: require('./graphiteWorking.check')
