abdee25dded38c055b5c3022d56ce9fa274199e2
fahad.khan <fahad.khan@ft.com>
2016-08-22 12:30:47 +0100

    Include a plain/text response string check
    This can be used for service that implement the FT __gtg standard.

---
+++ test/fixtures/config/stringCheckFixture.js
@@ -0,0 +1,23 @@
+'use strict';
+module.exports = {
+	name: 'string check fixture',
+	description: '',
+	checks: [
+		{
+			type: 'string',
+			name: 'test1',
+			url: 'http://pretendurl.com',
+			expected: 'OK',
+			severity: 2,
+			businessImpact: 'blah',
+			technicalSummary: 'god knows',
+			panicGuide: 'Don\'t Panic',
+			checkResult: {
+				PASSED: 'Text if check passed',
+				FAILED: 'Text is check failed',
+				PENDING: 'This check has not yet run'
+			},
+			interval: '1s',
+		}
+	]
+};
