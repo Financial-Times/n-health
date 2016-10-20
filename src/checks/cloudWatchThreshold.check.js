f97222ded3ecd71c2fbc8109630d77bf92d03f17
phamann <patrickhamann@gmail.com>
2016-10-20 15:20:35 +0100

    Add new check type for CloudWatch threshold measurement

---
+++ src/checks/cloudWatchThreshold.check.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const moment = require('moment');
+const status = require('./status');
+const Check = require('./check');
+
+// Detects when the value of a metric climbs above/below a threshold value
+class CloudWatchThresholdCheck extends Check {
+
+	constructor(options){
+		super(options);
+
+		this.threshold = options.threshold;
+		this.direction = options.direction || 'above';
+		this.samplePeriod = parseInt(options.samplePeriod, 10) || 60 * 5;
+
+		this.cloudWatchRegion = options.cloudWatchRegion || 'eu-west-1';
+		this.cloudWatchMetricName = options.cloudWatchMetricsName;
+		this.cloudWatchNamespace = options.cloudWatchNamespace;
+		this.cloudWatchStatistic = options.cloudWatchStatistic || 'Sum';
+		this.cloudWatchDimensions = options.cloudWatchDimensions || [];
+
+		this.cloudWatch = new AWS.CloudWatch({region: this.cloudWatchRegion, apiVersion: '2010-08-01'});
+
+		this.checkOutput = 'CloudWatch threshold check has not yet run';
+	}
+
+	generateParams() {
+		return  {
+			EndTime: moment().toISOString(),
+			StartTime: moment().subtract(this.samplePeriod, 'seconds').toISOString(),
+			MetricName: this.cloudWatchMetricName,
+			Namespace: this.cloudWatchNamespace,
+			Period: this.samplePeriod,
+			Statistics: [this.cloudWatchStatistic],
+			Dimensions: this.cloudWatchDimensions
+		};
+	}
+
+	tick() {
+		const params = this.generateParams();
+
+		return this.cloudWatch
+				.getMetricStatistics(params)
+				.promise()
+				.then(res => {
+					const value = res.Datapoints
+									.map(datum => datum[this.cloudWatchStatistic])
+									.reduce((a, b) => a + b, 0);
+					let ok;
+
+					if (this.direction === 'above') {
+						ok = value <= this.threshold;
+					} else {
+						ok = value >= this.threshold;
+					}
+
+					this.status = ok ? status.PASSED : status.FAILED;
+					this.checkOutput = ok ? 'No threshold change detected in CloudWatch data' : `CloudWatch data ${this.direction} required threshold`;
+				})
+				.catch(err => {
+					console.error('Failed to get CloudWatch data', err);
+					this.status = status.FAILED;
+					this.checkOutput = `Cloudwatch threshold check failed to fetch data: ${err.message}`;
+				});
+	}
+}
+
+module.exports = CloudWatchThresholdCheck;
