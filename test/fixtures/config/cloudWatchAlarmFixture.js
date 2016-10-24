b414a42cd6ac03f1eff5e4194b4c96dde0c0cd4e
phamann <patrickhamann@gmail.com>
2016-10-24 12:35:43 +0100

    Add tests for new CloudWatch Alarm check

---
+++ test/fixtures/config/cloudWatchAlarmFixture.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = {
+	name: 'cloudwatch',
+	descriptions : '',
+	checks : [
+		{
+			type: 'cloudWatchAlarm',
+			cloudWatchAlarmName: 'test',
+			name: 'test',
+			severity: 2,
+			businessImpact: 'catastrophic',
+			technicalSummary: 'god knows',
+			panicGuide: 'Don\'t Panic'
+		}
+	]
+};
