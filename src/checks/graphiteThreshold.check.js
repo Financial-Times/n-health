e7546b85bbe8d1f6f347232d894327d3550f89a1
Andy Gout <andygout@users.noreply.github.com>
2016-12-14 15:49:13 +0000

    Remove: minSeries + maxSeries in threshold check (#39)
    * Remove: minSeries + maxSeries in threshold check (+ refactor accordingly)
    
    * Remove: Tests checking that minSeries + maxSeries are used in Graphite functions to acquire metrics
    
    * Amend: Mock data used in tests to reflect format of real data
    
    * Improve: Efficiency by using 'some' function to process sample datapoints
    
    * Amend: Mock data to make values more varied
    
    * Dispense: Mock data arguments used for test to configure sample period

--- src/checks/graphiteThreshold.check.js
+++ src/checks/graphiteThreshold.check.js
@@ -32,9 +32,7 @@ class GraphiteThresholdCheck extends Check {
 	}
 
 	generateUrl(metric, period) {
-		const urlBase = this.graphiteBaseUrl + `format=json&from=-${period}&target=`;
-		const functionName = this.direction === 'above' ? 'maxSeries' : 'minSeries';
-		return urlBase + `${functionName}(${metric})`;
+		return this.graphiteBaseUrl + `format=json&from=-${period}&target=` + metric;
 	}
 
 	tick(){
@@ -42,18 +40,16 @@ class GraphiteThresholdCheck extends Check {
 		return fetch(this.sampleUrl)
 			.then(fetchres.json)
 			.then(sample => {
-				const datapoints = sample[0].datapoints;
-
-				const invalidDatapoints = datapoints.filter(value => {
-					return this.direction === 'above' ?
-						value[0] && value[0] > this.threshold :
-						value[0] && value[0] < this.threshold;
+				const failed = sample.some(result => {
+					return result.datapoints.some(value => {
+						return this.direction === 'above' ?
+							value[0] && value[0] > this.threshold :
+							value[0] && value[0] < this.threshold;
+					});
 				});
 
-				const ok = !invalidDatapoints.length;
-
-				this.status = ok ? status.PASSED : status.FAILED;
-				this.checkOutput = ok ? 'No spike detected in graphite data' : 'Spike detected in graphite data';
+				this.status = failed ? status.FAILED : status.PASSED;
+				this.checkOutput = failed ? 'Spike detected in graphite data' : 'No spike detected in graphite data';
 			})
 			.catch(err => {
 				console.error('Failed to get JSON', err);
