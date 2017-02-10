9ae5b06eb0388f55645606de0a429eed4dc32b6d
Ben Fletcher <bjfletcher@gmail.com>
2017-02-10 14:35:22 +0000

    add some comparison options (#50)

--- test/responseCompare.check.spec.js
+++ test/responseCompare.check.spec.js
@@ -3,7 +3,7 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
 const proxyquire = require('proxyquire').noCallThru().noPreserveCache();
-const config = require('./fixtures/config/responseCompareFixture').checks[0];
+const config = require('./fixtures/config/responseCompareFixture').checks;
 
 describe('Response Compare Check', function(){
 
@@ -11,7 +11,7 @@ describe('Response Compare Check', function(){
 	let ResponseCompareCheck;
 	let mockFetch;
 
-	function setup (bodies) {
+	function setup (bodies, config) {
 		mockFetch = sinon.stub();
 		bodies.forEach((body, i) => {
 			mockFetch.onCall(i).returns(Promise.resolve({
@@ -27,7 +27,7 @@ describe('Response Compare Check', function(){
 	describe('equal', function(){
 
 		it('Will pass if the 2 responses are the same', function(done){
-			const check = setup(['hip', 'hip']);
+			const check = setup(['hip', 'hip'], config[0]);
 			check.start();
 			setImmediate(function(){
 				sinon.assert.called(mockFetch);
@@ -38,7 +38,29 @@ describe('Response Compare Check', function(){
 		});
 
 		it('Will fail if the 2 responses are different', function(done){
-			const check = setup(['hur', 'rah']);
+			const check = setup(['hur', 'rah'], config[0]);
+			check.start();
+			setImmediate(function(){
+				sinon.assert.called(mockFetch);
+				expect(check.getStatus().ok).to.be.false;
+				check.stop();
+				done();
+			});
+		});
+
+		it('Will pass if the two normalized responses are the same', function(done){
+			const check = setup(['hip 1234', 'hip dddd'], config[1]);
+			check.start();
+			setImmediate(function(){
+				sinon.assert.called(mockFetch);
+				expect(check.getStatus().ok).to.be.true;
+				check.stop();
+				done();
+			});
+		});
+
+		it('Will fail if the two normalized responses are different', function(done){
+			const check = setup(['hip rahh', 'hip dddd'], config[1]);
 			check.start();
 			setImmediate(function(){
 				sinon.assert.called(mockFetch);
