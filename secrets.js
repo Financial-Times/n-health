66e5e7622480466481de5985c5698f0b4cddae43
Andy Gout <andygout@users.noreply.github.com>
2017-04-27 13:12:18 +0100

    Add: Secret Squirrel (#62)

---
+++ secrets.js
@@ -0,0 +1,30 @@
+module.exports = {
+	whitelist: [
+		'ldbchjvwdc65gbj8grn1xuemlxrq487i', // pingdom.check.js
+		'23077025-960d-11e6-afcc-07964ee4b5be', // cloudWatchThresholdResponse.json
+		'f8585BOxnGQDMbnkJoM1e', // graphiteWorkingFixture.js
+		'sendnotificationwhendown', // pingdomDownResponse.json
+		'pingdomUpResponse.json', // pingdomUpResponse.json
+		'01e01de3c6ca456b87da5278bc6c6e36', // sauceLabsFailedResponse.json + sauceLabsPassedResponse.json
+		'736ee205-b510-40bc-b58f-7b97709559a8', // serviceRegistryFixture.json
+		'configure/service/3GQMFG1fYBJojRtN2fWQMX', // serviceRegistryFixture.json
+		'a8527323-7e3a-4f67-8636-2117426f17ad', // serviceRegistryFixture.json
+		'4e745372-f6b4-4014-a07c-ddaaf1f62d1b', // serviceRegistryFixture.json
+		'c7bd8f6b-7b8d-4163-81c1-51a9049c2518', // serviceRegistryFixture.json
+		'd1a7143a-f8a6-42fe-bbc5-3fa8cc0078b4', // serviceRegistryFixture.json
+		'a9554544-68b2-416e-87e5-e46bbc60c9df', // serviceRegistryFixture.json
+		'a09e13e4-72f9-40ba-881e-d01ce03d420d', // serviceRegistryFixture.json
+		'8c39cd07-ac75-4bde-9b99-eb8e615d8121', // serviceRegistryFixture.json
+		'876e69a7-349f-4b40-b203-d1b0e6fadb15', // serviceRegistryFixture.json
+		'cd950eb2-085e-402b-94a0-2f1235599ee8', // serviceRegistryFixture.json
+		'1941c1f7-dfba-4068-9fa2-42e5d3888c88', // serviceRegistryFixture.json
+		'5d31735a-8fd2-4107-b456-1319ab834802', // serviceRegistryFixture.json
+		'bab7096a-3981-4f7e-8637-6daff95bf5d0', // serviceRegistryFixture.json
+		'3f75086b-efed-4573-a25c-f92db56a3bdf', // serviceRegistryFixture.json
+		'5adf06c5-608c-4807-a4f1-1c16f916e7b5', // serviceRegistryFixture.json
+		'4cedd067-21f9-4907-b1f9-d06da1da24de', // serviceRegistryFixture.json
+		'b191f395-f2dd-4b4e-a44e-d5af4ae8d09c', // serviceRegistryFixture.json
+		'a982bca5-e90d-45de-8d57-97c190c71bef', // serviceRegistryFixture.json
+		'be91fac7-5b0e-40f5-abd1-b81b72ad1b97' // serviceRegistryFixture.json
+	]
+};
