3c25b08fe6abc9d40b91267053b14b94b8b9ccc0
Rémy Bach <remy@bach.me.uk>
2018-01-17 11:28:28 +0000

    Fix slightly misleading graphite check error (#74)
    * Fix slightly misleading graphite check error
    
    * Add healthcheck name to error logs

--- src/checks/graphiteThreshold.check.js
+++ src/checks/graphiteThreshold.check.js
@@ -25,9 +25,13 @@ class GraphiteThresholdCheck extends Check {
 		if (!this.ftGraphiteKey) {
 			throw new Error('You must set FT_GRAPHITE_KEY environment variable');
 		}
+		
+		if (!options.metric) {
+			throw new Error(`You must pass in a metric for the "${options.name}" check - e.g., "next.heroku.article.*.express.start"`);
+		}
 
-		if (!options.metric || !options.metric.match(/next\./)) {
-			throw new Error(`You must prepend the metric (${options.metric}) with "next." - e.g., "heroku.article.*.express.start" needs to be "next.heroku.article.*.express.start"`);
+		if (!/^next\./.test(options.metric)) {
+			throw new Error(`You must prepend the metric (${options.metric}) with "next." for the "${options.name}" check - e.g., "heroku.article.*.express.start" needs to be "next.heroku.article.*.express.start"`);
 		}
 		this.metric = options.metric;
 
