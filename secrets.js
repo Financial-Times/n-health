9bd85a57d56c04ab31bda675aaf9cf37472e2fe5
circle-auto-deploy <estefania.morton@ft.com>
2021-05-18 15:10:21 +0100

    remove references to serviceRegistryFixture from secrets

--- secrets.js
+++ secrets.js
@@ -1,26 +1,6 @@
 module.exports = {
 	whitelist: [
 		'23077025-960d-11e6-afcc-07964ee4b5be', // test/fixtures/cloudWatchThresholdResponse.json
-		'f8585BOxnGQDMbnkJoM1e', // test/fixtures/config/graphiteWorkingFixture.js
-		'736ee205-b510-40bc-b58f-7b97709559a8', // test/fixtures/serviceRegistryFixture.json
-		'configure/service/3GQMFG1fYBJojRtN2fWQMX', // test/fixtures/serviceRegistryFixture.json
-		'a8527323-7e3a-4f67-8636-2117426f17ad', // test/fixtures/serviceRegistryFixture.json
-		'4e745372-f6b4-4014-a07c-ddaaf1f62d1b', // test/fixtures/serviceRegistryFixture.json
-		'c7bd8f6b-7b8d-4163-81c1-51a9049c2518', // test/fixtures/serviceRegistryFixture.json
-		'd1a7143a-f8a6-42fe-bbc5-3fa8cc0078b4', // test/fixtures/serviceRegistryFixture.json
-		'a9554544-68b2-416e-87e5-e46bbc60c9df', // test/fixtures/serviceRegistryFixture.json
-		'a09e13e4-72f9-40ba-881e-d01ce03d420d', // test/fixtures/serviceRegistryFixture.json
-		'8c39cd07-ac75-4bde-9b99-eb8e615d8121', // test/fixtures/serviceRegistryFixture.json
-		'876e69a7-349f-4b40-b203-d1b0e6fadb15', // test/fixtures/serviceRegistryFixture.json
-		'cd950eb2-085e-402b-94a0-2f1235599ee8', // test/fixtures/serviceRegistryFixture.json
-		'1941c1f7-dfba-4068-9fa2-42e5d3888c88', // test/fixtures/serviceRegistryFixture.json
-		'5d31735a-8fd2-4107-b456-1319ab834802', // test/fixtures/serviceRegistryFixture.json
-		'bab7096a-3981-4f7e-8637-6daff95bf5d0', // test/fixtures/serviceRegistryFixture.json
-		'3f75086b-efed-4573-a25c-f92db56a3bdf', // test/fixtures/serviceRegistryFixture.json
-		'5adf06c5-608c-4807-a4f1-1c16f916e7b5', // test/fixtures/serviceRegistryFixture.json
-		'4cedd067-21f9-4907-b1f9-d06da1da24de', // test/fixtures/serviceRegistryFixture.json
-		'b191f395-f2dd-4b4e-a44e-d5af4ae8d09c', // test/fixtures/serviceRegistryFixture.json
-		'a982bca5-e90d-45de-8d57-97c190c71bef', // test/fixtures/serviceRegistryFixture.json
-		'be91fac7-5b0e-40f5-abd1-b81b72ad1b97' // test/fixtures/serviceRegistryFixture.json
+		'f8585BOxnGQDMbnkJoM1e' // test/fixtures/config/graphiteWorkingFixture.js
 	]
 };
