94f62fcb0c5891fa7602745dfe7e1b6f681fa5b0
Juan Sanchez Alcala <102036944+juanSanchezAlcala@users.noreply.github.com>
2023-06-21 12:44:46 +0200

    add overrides for weekend on graphiteWorking check

--- test/graphiteworking.spec.js
+++ test/graphiteworking.spec.js
@@ -1,12 +1,12 @@
 'use strict';
 const expect = require('chai').expect;
 const sinon = require('sinon');
-const proxyquire = require('proxyquire').noCallThru().noPreserveCache();
+const proxyquire = require('proxyquire').noCallThru().noPreserveCache();	
 
 describe('Graphite Working Check', function(){
-
-	const fixture = require('./fixtures/config/graphiteWorkingFixture').checks[0];
-
+	const fixtures = require('./fixtures/config/graphiteWorkingFixture');
+	const fixtureBasic = fixtures.checks[0];
+	const fixtureWeekend = fixtures.checks[1];
 	let GraphiteWorkingCheck;
 	let check;
 	let mockFetch;
@@ -57,7 +57,7 @@ describe('Graphite Working Check', function(){
 		return new Promise(resolve => setTimeout(resolve, time));
 	}
 
-	function setup(response){
+	function setup(response,fixture = fixtureBasic){
 		mockFetch = sinon.stub().returns(Promise.resolve({
 			ok: true,
 			json: function(){
@@ -75,7 +75,7 @@ describe('Graphite Working Check', function(){
 		return waitFor(10).then(() => {
 			sinon.assert.called(mockFetch);
 			let url = mockFetch.lastCall.args[0];
-			expect(url).to.contain(fixture.metric);
+			expect(url).to.contain(fixtureBasic.metric);
 			expect(url).to.contain('format=json');
 		});
 	});
@@ -106,7 +106,36 @@ describe('Graphite Working Check', function(){
             expect(check.getStatus().checkOutput).to.equal('summarize(next.fastly.133g5BGAc00Hv4v8t0dMry.anzac.requests, "1h", "sum", true) has been null for Infinity minutes.');
         });
     });
+	
+	describe("Applying overrides", () => {
+	
+		it('Should override parameters while weekend',async () => {
+			setup(goodResponse,fixtureWeekend)
+			//mock to be weekend
+			check.isWeekend = sinon.stub().returns(() => true);
+			check.start();
+			await waitFor(10);
+			sinon.assert.called(check.isWeekend );
+			sinon.assert.called(mockFetch);
+			expect(check.getStatus().checkOutput).to.equal('next.fastly.f8585BOxnGQDMbnkJoM1e.weekend.requests has data');
+			expect(check.time).to.equal(fixtureWeekend.override.weekend.time);
+			expect(check.metric).to.equal(fixtureWeekend.override.weekend.metric)
+			expect(check.initialState.metric).to.equal(fixtureWeekend.metric);
+			expect(check.initialState.time).to.equal(fixtureWeekend.time)
+			expect(check.getStatus().ok).to.be.true;
+			check.isWeekend = sinon.stub().returns(() => false);
 
+			await waitFor(10);
+			sinon.assert.calledTwice(mockFetch);
+			sinon.assert.called(check.isWeekend );
+			expect(check.getStatus().checkOutput).to.equal('next.fastly.f8585BOxnGQDMbnkJoM1e.all.requests has data');
+			expect(check.time).to.equal(fixtureWeekend.time);
+			expect(check.metric).to.equal(fixtureWeekend.metric)
+			expect(check.initialState.metric).to.equal(fixtureWeekend.metric);
+			expect(check.initialState.time).to.equal(fixtureWeekend.time)
+			expect(check.getStatus().ok).to.be.true;
+		})
+	})
 	//todo get the graphite api key into the CI config - doesn't seem possible right now...
 	describe.skip('Integration', function(){
 
